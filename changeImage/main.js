e3d79e00de417bb511f5cb1f54e682634e4f3945
ethan <izawaryosuke0101@gmail.com>
2020-10-21 19:04:13 +0900

    changeImage

---
+++ changeImage/main.js
@@ -0,0 +1,7 @@
+const thumbs = document.querySelectorAll('.thumb');
+thumbs.forEach(function(item, index){
+  item.onclick = function(){
+    document.getElementById('bigimg').src = this.dataset.image;
+  }
+});
+
